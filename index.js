377141aa2a6bf10621ffa996aa48d505f3d1c43f
mohd usman <usman12797@gmail.com>
2023-11-15 17:48:34 +0530

    pathmodule

---
+++ index.js
@@ -0,0 +1,8 @@
+//path module
+const path=require("path");
+console.log(path.basename("E:\nodePracticle\Pathmodule\index.html"));
+console.log(path.dirname("E:\nodePracticle\Pathmodule\index.html"));
+//extname()- return file extension
+console.log(path.extname("E:\nodePracticle\Pathmodule\index.html"));
+console.log(path.extname("E:\nodePracticle\Pathmodule\index.css"));
+console.log(path.extname("E:\nodePracticle\Pathmodule\index"));
